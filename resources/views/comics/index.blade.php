efa0fc95a11db1320fb6e903f91780c90d59af68
sder12 <sara.cetto@gmail.com>
2022-12-26 17:42:10 +0100

    index table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,13 +1,55 @@
 @extends('layouts.app')
 
-@section('title', 'Comics')
+@section('title', 'Index')
 
 @section('content')
-   
-   <h1>Comics</h1>
-   <ul>
-   @foreach ($comics as $comic)
-       <li> {{ $comic->title}}</li>
-   @endforeach
-    </ul>
+
+    <section>
+        <div class="container py-2">
+            <h2 class="text-center">All Comics </h2>
+        </div>
+    </section>
+
+    <section class="text-end">
+        <a href="#" class="btn  btn-outline-dark">create new comic</a>
+    </section>
+
+    <section>
+        <div class="container mt-4">
+
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Series</th>
+                        <th scope="col">Sale date</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                        <tr>
+                            <th scope="row">{{ $comic->title }}</th>
+                            <td>{{ $comic->type }}</td>
+                            <td>{{ $comic->series }}</td>
+                            <td>{{ $comic->sale_date }}</td>
+                            <td>
+                                <a href="#" class="btn btn-success">
+                                    <i class="fa-solid fa-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-warning">
+                                    <i class="fa-solid fa-pencil"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger">
+                                    <i class="fa-solid fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+
 @endsection
