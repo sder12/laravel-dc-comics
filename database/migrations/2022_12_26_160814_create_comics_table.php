ea08b7064615441eeee6ac1850b1a55ea4b6b86e
sder12 <sara.cetto@gmail.com>
2022-12-26 17:15:54 +0100

    create migration

---
+++ database/migrations/2022_12_26_160814_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 150);
+            $table->text('description')->nullable();
+            $table->text('thumb');
+            $table->string('price', 20);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 50);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
